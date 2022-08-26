012ae71dc644606c4706c827affd686d2b031f75
Arbusta <morinigo.matias@arbusta.net>
2022-08-26 09:14:13 -0300

    lista

---
+++ Listas/Matías Morinigo/Listas.py
@@ -0,0 +1,59 @@
+# 1 Escriba un programa que permita crear una lista de palabras. Para ello, el programa tiene que pedir un número y 
+# luego solicitar ese número de palabras para crear la lista. Por último, el programa tiene que escribir la lista.
+
+# num = int(input("Escriba un numero: "))
+# can = input("Escriba la cantidad de palabras segun el numero ingresado: ")
+# l1 = [can]
+# print(l1)
+
+
+
+# 2 Escriba un programa que permita crear una lista de palabras y que, a continuación, pida una palabra y diga cuántas 
+# veces aparece esa palabra en la lista.
+
+# lista = input("Ingrese una lista: ")
+# l1 = [lista]
+# print(l1)
+# palaPed = input("Ingrese una palabra de la lista: ")
+# l1 = lista.count(palaPed)
+# print(l1)    
+
+
+
+# 3 Escriba un programa que permita crear una lista de palabras y que, a continuación, pida dos palabras y sustituya la 
+# primera por la segunda en la lista.
+
+# lista1 = input("Ingrese una lista: ")
+# l1 = [lista1]
+# print(l1)
+# lista2 = input("Ingrese dos palabras: ")
+# lista1[1] = [lista2]
+# print(lista1)
+
+
+# lista1 = ["manzana", "pera", "frutilla"]
+# print(lista1)
+# lista2 = input("Ingrese dos palabras: ")
+# l1= lista1.insert(0, lista2)
+# print(lista1)
+
+
+
+# 4 Escriba un programa que permita crear una lista de palabras y que, a continuación, pida una palabra y elimine esa 
+# palabra de la lista.
+
+preg = ["manzana", "pera", "banana", "ciruela", "uva", "cereza"]
+print(preg)
+preg2 = input("Ingrese una palabra de la lista para eliminar: ")
+lista = preg.remove(preg2)
+print(lista)
+
+
+# preg = input("Ingrese una lista: ")
+# l1 = [preg]
+# print(l1)
+# preg2 = input("Ingrese una palabra de la lista para eliminar: ")
+# for i in l1:
+#     l2 = preg.remove(len(str(preg2)))
+#     print(preg)
+
